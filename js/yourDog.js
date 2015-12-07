301970d0f6400f1c6377f1711467fb1deeca87b9
Tim Malesky <timmalesky@Tims-MBP.fios-router.home>
2015-12-07 11:38:44 -0500

    Happy dog image added

--- js/yourDog.js
+++ js/yourDog.js
@@ -6,7 +6,62 @@
 
 var myId = document.location.search.split('=')[1];
 var dog;
+var id_of_med_to_update;
+var id_of_app_to_update;
+var id_of_vac_to_update;
+var id_of_die_to_update;
 //xhr.open('GET','http://dog-tracker-api.herokuapp.com/dogs/' + myId, true);
+
+function editAppointment(appointment) {
+    console.log(appointment);
+    id_of_app_to_update = appointment.id;
+    $('#appointment_date').val(appointment.appointment_date);
+    $('#location').val(appointment.location);
+    $('#vet_name').val(appointment.vet_name);
+    $('.modal-edit-appointment').modal({show: true});
+}
+
+function hideEditAppointment() {
+    $('.modal-edit-appointment').modal('hide');
+}
+
+function editMedication(medication) {
+    console.log(medication);
+    id_of_med_to_update = medication.id;
+    $('#med_name').val(medication.name);
+    $('#dose').val(medication.dose);
+    $('#med_comment').val(medication.comment);
+    $('.modal-edit-medication').modal({show: true});
+}
+
+function hideEditMedication() {
+    $('.modal-edit-medication').modal('hide');
+}
+
+function editVaccination(vaccination) {
+    console.log(vaccination);
+    id_of_vac_to_update = vaccination.id;
+    $('#vacc_name').val(vaccination.name);
+    $('#last').val(vaccination.last);
+    $('#next').val(vaccination.next);
+    $('.modal-edit-vaccination').modal({show: true});
+}
+
+function hideEditVaccination() {
+    $('.modal-edit-vaccination').modal('hide');
+}
+
+function editDietaryRestriction(dietary_restrictions) {
+    console.log(dietary_restrictions);
+    id_of_die_to_update = dietary_restrictions.id;
+    $('#food_name').val(dietary_restrictions.food_name);
+    $('#comment').val(dietary_restrictions.comment);
+    $('.modal-edit-dietary-restrictions').modal({show: true});
+}
+
+function hideEditDietaryRestriction() {
+    $('.modal-edit-dietary-restrictions').modal('hide');
+}
 
 $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
     function(data){
@@ -38,6 +93,12 @@ $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
             newSpan1.innerHTML = new Date(dog.appointments[i].appointment_date).toDateString();
             newDiv.appendChild(newLabel1);
             newDiv.appendChild(newSpan1);
+            containerDiv.onclick =(function() {
+                var temp_appointment = dog.appointments[i];
+                return function() {
+                    editAppointment(temp_appointment);
+                }
+            })();
 
             var newDiv = document.createElement("div");
             containerDiv.appendChild(newDiv);
@@ -63,6 +124,7 @@ $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
             var containerDiv = document.createElement("div");
             containerDiv.className = "items-container";
             vaccinations.appendChild(containerDiv);
+
             var newDiv = document.createElement("div");
             containerDiv.appendChild(newDiv);
             var newLabel1 = document.createElement("label");
@@ -71,6 +133,12 @@ $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
             newSpan1.innerHTML = dog.vaccinations[v].name;
             newDiv.appendChild(newLabel1);
             newDiv.appendChild(newSpan1);
+            containerDiv.onclick =(function() {
+                var temp_vaccination = dog.vaccinations[v];
+                return function() {
+                    editVaccination(temp_vaccination);
+                }
+            })();
 
             var newDiv = document.createElement("div");
             containerDiv.appendChild(newDiv);
@@ -96,6 +164,7 @@ $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
             var containerDiv = document.createElement("div");
             containerDiv.className = "items-container";
             dietary_restrictions.appendChild(containerDiv);
+
             var newDiv = document.createElement("div");
             containerDiv.appendChild(newDiv);
             var newLabel1 = document.createElement("label");
@@ -104,6 +173,12 @@ $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
             newSpan1.innerHTML = dog.dietary_restrictions[d].food_name;
             newDiv.appendChild(newLabel1);
             newDiv.appendChild(newSpan1);
+            containerDiv.onclick =(function() {
+                var temp_dietary_restrictions = dog.dietary_restrictions[d];
+                return function() {
+                    editDietaryRestriction(temp_dietary_restrictions);
+                }
+            })();
 
             var newDiv = document.createElement("div");
             containerDiv.appendChild(newDiv);
@@ -114,7 +189,7 @@ $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
             newDiv.appendChild(newLabel1);
             newDiv.appendChild(newSpan1);
         }
-        //$
+
         var medications = $("#medBody")[0];
         for (var i = 0; i < dog.medications.length; i++ ) {
             var containerDivB = document.createElement("div");
@@ -129,6 +204,12 @@ $.get('http://dog-tracker-api.herokuapp.com/dogs/' + myId,
             newSpanB1.innerHTML = dog.medications[i].name;
             newDivB.appendChild(newLabelB1);
             newDivB.appendChild(newSpanB1);
+            containerDivB.onclick =(function() {
+                var temp_medication = dog.medications[i];
+                return function() {
+                    editMedication(temp_medication);
+                }
+            })();
 
             var newDivB = document.createElement("div");
             containerDivB.appendChild(newDivB);
@@ -192,3 +273,82 @@ $('#activateVaccs').click(function(){
 $('#activateDiet').click(function(){
     $("#dietBody").slideToggle();
 });
+
+function updateAppointment() {
+    var app = {};
+    app.appointment = {
+        date: $('#appointment_date')[0].value,
+        location: $('#location')[0].value,
+        vet_name: $('#vet_name')[0].value
+    };
+    $.ajax({
+        type: "PUT",
+        url: 'http://dog-tracker-api.herokuapp.com/appointments/' + id_of_app_to_update,
+        contentType: "application/json",
+        data: JSON.stringify(app),
+        success: function() {
+            hideEditAppointment();
+            window.location.reload();
+        }
+    });
+}
+
+
+function updateMedication() {
+    var med = {};
+    med.medication = {
+        name: $('#med_name')[0].value,
+        dose: $('#dose')[0].value,
+        comment: $('#med_comment')[0].value
+    };
+    $.ajax({
+        type: "PUT",
+        url: 'http://dog-tracker-api.herokuapp.com/medications/' + id_of_med_to_update,
+        contentType: "application/json",
+        data: JSON.stringify(med),
+        success: function() {
+            hideEditMedication();
+            window.location.reload();
+        }
+     });
+}
+
+    function updateVaccination() {
+    var vac = {};
+    vac.vaccination = {
+        name: $('#vacc_name')[0].value,
+        last: $('#last')[0].value,
+        next: $('#next')[0].value
+    };
+    $.ajax({
+        type: "PUT",
+        url: 'http://dog-tracker-api.herokuapp.com/vaccinations/' + id_of_vac_to_update,
+        contentType: "application/json",
+        data: JSON.stringify(vac),
+        success:function() {
+            hideEditVaccination();
+            window.location.reload();
+
+        }
+
+    });
+}
+
+function updateDietaryRestriction() {
+    var die = {};
+    die.dietary_restriction = {
+        food_name: $('#food_name')[0].value,
+        comment: $('#comment')[0].value
+    };
+    $.ajax({
+        type: "PUT",
+        url: 'http://dog-tracker-api.herokuapp.com/dietary_restrictions/' + id_of_die_to_update,
+        contentType: "application/json",
+        data: JSON.stringify(die),
+        success: function() {
+            hideEditDietaryRestriction();
+            window.location.reload();
+
+        }
+    });
+}
